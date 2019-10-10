7b48134cc67d562bdce57ca1d5694a0c24e8b7da
nivila-it19 <54268230+nivila-it19@users.noreply.github.com>
2019-10-10 14:37:41 +0530

    first commit

---
+++ arrayadd.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+void main() {
+    int n,a[n],i,sum=0;
+    printf("enter the numbers :");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+      {
+               printf("%d",sum);
+    }
+}
+
