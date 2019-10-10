2091ee99990ccde4fec1c9f9d36910f08e699efb
nivila-it19 <54268230+nivila-it19@users.noreply.github.com>
2019-10-10 14:33:52 +0530

    first commit

---
+++ acendesarray.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void main() {
+   int i,j,n,a[10],b;
+   printf("enter the numbers");
+   scanf("%d",&n);
+   {
+   for (i = 0; i < n; i++)
+   scanf("%d",&a[i]);
+   }
+   {
+    for (i = 0; i < n-1; i++)
+    {
+     for (j = i + 1; j < n; j++)
+     {
+       if (a[i] > a[j])
+       {
+             b = a[i];
+             a[i] = a[j];
+             a[j] = b;
+       }
+     }
+    }
+   }
+       printf("Ascending Order:\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d\n",a[i]);
+        }
+        printf("descending order:\n");
+        for(i = n-1; i >= 0; i--)
+        {
+            printf("%d\n",a[i]);
+        }
+
+}
+
